25852374e18477aebfb3f069ce3621311ed9e844
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-06-08 16:43:45 +0200

    Removed faulty position calculation

--- src/utils/Engine/directionVectors.js
+++ src/utils/Engine/directionVectors.js
@@ -19,19 +19,6 @@ const rebounceVectors = new Map(
   [vectors.downRight, vectors.upLeft],
 );
 
-function getBouncePosition(collidingObjPos, vector, bounceVector) {
-  const vectorPosition = vector(collidingObjPos);
-  const bounceVectorPosition = bounceVector(collidingObjPos);
-  const x = (vectorPosition[0] + bounceVectorPosition[0]) / 2;
-  const y = (vectorPosition[1] + bounceVectorPosition[1]) / 2;
-  return [x, y];
-  // example:
-  // pos [2,7]
-  // vector upLeft: x-1, y-1 => [1,6]
-  // bounce vector upRight: x-1, y+1 [1,8]
-  // result after calculation[1,7] => position after bounce
-}
-
 function getRandom() {
   const modifiers = Object.values(vectors);
   const randomIndex = Math.floor(Math.random() * modifiers.length);
@@ -39,4 +26,4 @@ function getRandom() {
 }
 
 export default vectors;
-export { getRandom, bounceVectors, getBouncePosition, rebounceVectors };
+export { getRandom, bounceVectors, rebounceVectors };
