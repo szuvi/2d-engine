6b50a17dc270067c011a3d7d3dc04acb079e8f7a
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-06-09 20:49:11 +0200

    Created factory component for Game Objects

---
+++ src/components/GameObject.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import * as React from 'react';
+import Static from './GameObjects/Static';
+import Teleport from './GameObjects/Teleport';
+import Vehicle from './GameObjects/Vehicle';
+import Base from './GameObjects/Base';
+
+function GameObject(props) {
+  switch (props.objectType) {
+    case 'static':
+      return <Static {...props} />;
+    case 'teleport':
+      return <Teleport {...props} />;
+    case 'moving':
+      return <Vehicle {...props} />;
+    case 'empty':
+      return <Base {...props} />;
+    default:
+      throw new Error('Incorrect Game Object type');
+  }
+}
+
+export default GameObject;
