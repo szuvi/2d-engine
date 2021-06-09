2d6d6b2034e96576e58e729081c7dbdb6c12dbc2
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-06-09 20:53:27 +0200

    Added logic for possible collision at start

--- src/utils/Engine/CollisionDetector.js
+++ src/utils/Engine/CollisionDetector.js
@@ -11,64 +11,32 @@ class CollisionDetector {
     this.inputConnection$ = null;
   }
 
-  connectSource$(positionRetrievingCb) {
+  connectSource$(positionRetrievingCb, entryVector, entryPosition) {
+    const startingState = CollisionDetector.getInitialState(
+      positionRetrievingCb,
+      entryVector,
+      entryPosition,
+    );
+
     return this.detectorsInputObservable$.pipe(
       map((position) => {
-        const objectAtPos =
-          position == null ? null : positionRetrievingCb(position);
+        const objectAtPos = positionRetrievingCb(position);
         return { id: uuidv4(), position, objectAtPos };
       }),
-      filter(({ objectAtPos }) => objectAtPos != null),
-      startWith({ id: uuidv4(), change: false }),
+      filter(({ objectAtPos }) => objectAtPos.type !== 'empty'),
+      startWith(startingState),
     );
+  }
+
+  static getInitialState(positionRetrievingCb, initialVector, initialPosition) {
+    const entryDetectorsPosition = initialVector(initialPosition);
+    const entryObjectAtPos = positionRetrievingCb(entryDetectorsPosition);
+    return {
+      id: uuidv4(),
+      position: entryDetectorsPosition,
+      objectAtPos: entryObjectAtPos,
+    };
   }
 }
 
 export default CollisionDetector;
-
-// connectSource$(positionRetrievingCb, observer) {
-
-//   this.inputConnection$ = this.detectorsInputObservable.subscribe(
-//     (position) => {
-//       const objectAtPos = positionRetrievingCb(position);
-//       if (objectAtPos !== null) {
-//         observer.next({ position, objectAtPos });
-//       }
-//     },
-//   );
-// }
-
-// disconnect() {
-//   this.inputConnection$.unsubscribe();
-// }
-// const CollisionDetecor = (function CollisionDetecorIIFE() {
-//   let positionDetector; // is supplied with position changes by next method
-
-//   const detectorsInputObservable = new Observable((subscriber) => {
-//     positionDetector = subscriber;
-//   });
-
-//   let inputConnection$;
-//   const connectSource = (retriever, recivingObserver) => {
-//     // connect here function to retrieve object from board
-//     inputConnection$ = detectorsInputObservable.subscribe((position) => {
-//       const objectAtPos = retriever(position);
-//       if (objectAtPos !== null) {
-//         recivingObserver.next({ position, objectAtPos });
-//       }
-//     });
-//   };
-
-//   const disconnect = () => inputConnection$.unsubscribe();
-
-//   return {
-//     connectSource,
-//     positionDetector,
-//     disconnect,
-//   };
-// })();
-
-// at pos change in ship => retrieve obj at pos based on provided function
-
-// connect to ship, each time there is a pos/vector/state change trigger collision detector (detector.next(pos, vector))
-// detector ...
