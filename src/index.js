d78e2a3404c74d97e23014adf66b94c40a561d21
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-06-09 20:57:09 +0200

    Connected main parts with Router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BoardProvider } from './contexts/BoardProvider';
+import SimpleMain from './containers/SimpleMain';
+import SimpleIntro from './containers/SimpleIntro';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <BoardProvider>
+        <Switch>
+          <Route path="/main">
+            <SimpleMain />
+          </Route>
+          <Route path="/">
+            <SimpleIntro />
+          </Route>
+          <Route path="*">
+            <p>404</p>
+          </Route>
+        </Switch>
+      </BoardProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
