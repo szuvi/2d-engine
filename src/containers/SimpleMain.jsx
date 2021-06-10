f3ba7629e6b029d4f66b8dce938b4d379c9fb223
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-06-10 06:17:00 +0200

    Fixed useEffect eslint error

--- src/containers/SimpleMain.jsx
+++ src/containers/SimpleMain.jsx
@@ -23,6 +23,7 @@ function SimpleMain() {
     return () => {
       subscription.unsubscribe();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleStart = () => {
