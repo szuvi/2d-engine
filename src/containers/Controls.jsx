6149da91bb5ec2dad51107e37d942d54a749e144
Konrad Trzciński <trzcinskiphoto@gmail.com>
2021-06-13 13:57:23 +0200

    Improved performance with useMemo and useCallback

--- src/containers/Controls.jsx
+++ src/containers/Controls.jsx
@@ -4,35 +4,47 @@ import ClearButton from '../components/ClearButton';
 import Row from '../components/Row';
 
 function Controls({ started, handleStart, handleStop, handleDirectionChange }) {
-  const changeDirection = (e) => {
-    handleDirectionChange(e.target.name);
-  };
+  const changeDirection = React.useCallback(
+    (e) => handleDirectionChange(e.target.name),
+    [handleDirectionChange],
+  );
 
-  return (
-    <Row>
-      <ClearButton onClick={handleStart} disabled={started}>
-        Start
-      </ClearButton>
-      <ClearButton onClick={handleStop} disabled={!started}>
-        Stop
-      </ClearButton>
-      <ClearButton onClick={changeDirection} disabled={started} name="upLeft">
-        ↖️
-      </ClearButton>
-      <ClearButton onClick={changeDirection} disabled={started} name="upRight">
-        ↗️
-      </ClearButton>
-      <ClearButton onClick={changeDirection} disabled={started} name="downLeft">
-        ↙️
-      </ClearButton>
-      <ClearButton
-        onClick={changeDirection}
-        disabled={started}
-        name="downRight"
-      >
-        ↘️
-      </ClearButton>
-    </Row>
+  return React.useMemo(
+    () => (
+      <Row>
+        <ClearButton onClick={handleStart} disabled={started}>
+          Start
+        </ClearButton>
+        <ClearButton onClick={handleStop} disabled={!started}>
+          Stop
+        </ClearButton>
+        <ClearButton onClick={changeDirection} disabled={started} name="upLeft">
+          ↖️
+        </ClearButton>
+        <ClearButton
+          onClick={changeDirection}
+          disabled={started}
+          name="upRight"
+        >
+          ↗️
+        </ClearButton>
+        <ClearButton
+          onClick={changeDirection}
+          disabled={started}
+          name="downLeft"
+        >
+          ↙️
+        </ClearButton>
+        <ClearButton
+          onClick={changeDirection}
+          disabled={started}
+          name="downRight"
+        >
+          ↘️
+        </ClearButton>
+      </Row>
+    ),
+    [started, handleStart, handleStop, changeDirection],
   );
 }
 
